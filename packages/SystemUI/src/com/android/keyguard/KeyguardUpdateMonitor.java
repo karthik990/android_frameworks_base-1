5dab0ea06ee48c7330b21faeaa5dc0aa7d52cca5
Mohan C M <mohanmanjappa@gmail.com>
2021-03-07 13:53:52 +0000

    Merge tag 'android-11.0.0_r33' of https://android.googlesource.com/platform/frameworks/base into dot11
    Android 11.0.0 Release 33 (RQ2A.210305.007)

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -2640,6 +2640,11 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
             return true;
         }
 
+        // change in battery overheat
+        if (current.health != old.health) {
+            return true;
+        }
+
         return false;
     }
 
